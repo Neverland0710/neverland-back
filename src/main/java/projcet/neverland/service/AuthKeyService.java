35e60acc9b8e9f5063b59558756e11c99c5e6bc7
정동연 <jdy789as@gmail.com>
2025-07-08 10:08:16 +0900

    fix : dockerfie, compose 수정

--- src/main/java/projcet/neverland/service/AuthKeyService.java
+++ src/main/java/projcet/neverland/service/AuthKeyService.java
@@ -1,13 +1,14 @@
 package projcet.neverland.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import projcet.neverland.dto.AuthKeyResponseDto;
 import projcet.neverland.entity.AuthKey;
 import projcet.neverland.repository.AuthKeyRepository;
 
 import java.util.Optional;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuthKeyService {
@@ -15,11 +16,17 @@ public class AuthKeyService {
     private final AuthKeyRepository authKeyRepository;
 
     public Optional<AuthKeyResponseDto> getAuthKeyInfo(String authCode) {
-        return authKeyRepository.findByAuthCode(authCode)
-                .map(authKey -> new AuthKeyResponseDto(
-                        authKey.getAuthKeyId(),
-                        authKey.getUserId(),
-                        authKey.getDeceasedId()
-                ));
+        String trimmedCode = authCode.trim();
+        log.info("🔍 조회 요청된 인증코드: '{}'", trimmedCode);
+
+        return authKeyRepository.findByAuthCode(trimmedCode)
+                .map(authKey -> {
+                    log.info("✅ 인증코드 일치: {}", authKey.getAuthKeyId());
+                    return new AuthKeyResponseDto(
+                            authKey.getAuthKeyId(),
+                            authKey.getUserId(),
+                            authKey.getDeceasedId()
+                    );
+                });
     }
 }
